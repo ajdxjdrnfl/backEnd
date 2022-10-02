e45b0a11cf6fafee4436f8f6f26ef6517795e966
ajdxjdrnfl <imreal4023@naver.com>
2022-10-02 20:19:47 +0900

    restapi

--- router/router.js
+++ router/router.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import {getList, delList, addList} from '../controller/todo.js'
+import {getList, delList, addList, makeList} from '../controller/todo.js'
 const router = express.Router();
 
 router.get('/:id/getList', getList);
 router.delete('/:id/delList', delList);
 router.post('/:id/addList', addList);
+router.post('/:id/makeList', makeList);
 
 export default router;
